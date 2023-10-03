5ca6d0956900855b5f773b11ceeacb3532150c89
FootballerMMA <114194273+FootballerMMA@users.noreply.github.com>
2023-10-03 00:09:34 -0500

    Updated battlescene
    Added card ui to scene

---
+++ Assets/Scripts/CardHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class CardHandler : MonoBehaviour
+{
+    void Start(){
+        Console.WriteLine("Started");
+    }
+    public void clickimage(){
+        Console.WriteLine("Clicked on card");
+    }
+}
