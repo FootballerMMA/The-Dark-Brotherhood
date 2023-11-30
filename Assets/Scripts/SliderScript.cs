3b5555ab08e953a1011f622079fcb0c91248adec
FootballerMMA <114194273+FootballerMMA@users.noreply.github.com>
2023-11-30 05:01:07 -0600

    Added Awake function in SliderScript

--- Assets/Scripts/SliderScript.cs
+++ Assets/Scripts/SliderScript.cs
@@ -10,33 +10,24 @@ public class SliderScript : MonoBehaviour
     [SerializeField] Text volumeLevel;
     [SerializeField] AudioMixer mixer;
     [SerializeField] RectTransform handle;
-
+    void Awake(){
+        if (!PlayerPrefs.HasKey("secondTime")){
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.SetInt("secondTime", 1);
+        }
+    }
     void Start(){
         float savedVolume;
-        //float volume;
         if (!PlayerPrefs.HasKey("Default Volume Changed")){
-            Debug.Log("No key!");
             savedVolume = -80.0f;
             PlayerPrefs.SetInt("Default Volume Changed", 1);
         } else {
-            Debug.Log("Key");
             savedVolume = PlayerPrefs.GetFloat("Master");
             float retrievedValue = Mathf.Pow(10, savedVolume / 20);
-            //Debug.Log("Receiving this saved " + savedVolume + " turned into " + retrievedValue);
-            //slideThing.value = savedVolume;
             slideThing.value = retrievedValue;
             volumeLevel.text = handle.anchorMin.x.ToString();
         }
         SetVolume("Master", savedVolume);
-        /*
-        if (PlayerPrefs.HasKey("Music")){
-            savedVolume = PlayerPrefs.GetFloat("Music");
-            Debug.Log("Received value " + savedVolume);
-            SetVolume("Music", savedVolume);
-        } else {
-            Debug.Log("Does not exist music key");
-        }
-        */
     }
     public void onValueChanged(){
         float sliderVal = slideThing.value;
@@ -48,14 +39,11 @@ public class SliderScript : MonoBehaviour
         }
         Debug.Log("Setting volume to " + volumeSetting);
         SetVolume("Master", volumeSetting);
-        //SetVolume("Music", volumeSetting);
         float adjustedVolume = handle.anchorMin.x;
         volumeLevel.text = adjustedVolume.ToString();
     }
 
     void SetVolume(string name, float value){
-        //Debug.Log("")
-        //Debug.Log("Setting mixer to " + value + " from " + code);
         PlayerPrefs.SetFloat(name, value);
         mixer.SetFloat(name, value);
     }
